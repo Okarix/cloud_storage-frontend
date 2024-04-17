017b815fffb319debe4a96147c06e04ab1169df0
Adiar <datkaevadiar@gmail.com>
2024-04-18 01:44:59 +0500

    fix: main page title text fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<WavyBackground className='max-w-4xl flex flex-col items-center'>
 			<p className='text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center'>Небо Ваших Файлов</p>
-			<p className='text-base md:text-lg mt-4 text-white font-normal inter-var text-center'>Где ваши данные находят свой дом в облаках.</p>
+			<p className='text-base md:text-lg mt-4 text-white font-normal inter-var text-center'>Храните. Делитесь. Вдохновляйтесь.</p>
 			<div className='flex flex-col sm:flex-row items-center justify-center gap-4 mt-6'>
 				<Link
 					href='/auth'
