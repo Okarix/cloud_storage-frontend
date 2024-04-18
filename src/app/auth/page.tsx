fb2be5142643a76e128eef4ef4b4401bae911970
Adiar <datkaevadiar@gmail.com>
2024-04-18 23:46:23 +0500

    ui: authpage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,3 +1,24 @@
+import { Login } from '../ui/login';
+import { SignUp } from '../ui/sign-up';
+import { Tabs } from '../ui/tabs';
+
 export default function Auth() {
-	return <h1 className='text-white'>Auth</h1>;
+	const tabs = [
+		{
+			title: 'Войти',
+			value: 'login',
+			content: <Login />,
+		},
+		{
+			title: 'Зарегистрироваться',
+			value: 'sign-up',
+			content: <SignUp />,
+		},
+	];
+
+	return (
+		<div className='h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-10'>
+			<Tabs tabs={tabs} />
+		</div>
+	);
 }
