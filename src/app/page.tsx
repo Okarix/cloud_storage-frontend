bdbd0c98e5e4740eaf5c9fb464292cd8e05d0cd2
Adiar <datkaevadiar@gmail.com>
2024-04-22 19:11:49 +0500

    fix: name changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
 	return (
 		<WavyBackground className='max-w-4xl flex flex-col items-center'>
-			<p className='text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center'>Небо Ваших Файлов</p>
+			<p className='text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center'>Облако Ваших Файлов</p>
 			<p className='text-base md:text-lg mt-4 text-white font-normal inter-var text-center'>Храните. Делитесь. Вдохновляйтесь.</p>
 			<div className='flex flex-col sm:flex-row items-center justify-center gap-4 mt-6'>
 				<Link
